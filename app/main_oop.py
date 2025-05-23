053505d5bf84e429986f1c85a9da466faf6653bd
Walter462 <74890413+Walter462@users.noreply.github.com>
2025-05-23 12:54:59 +0800

    ref(main_oop.py): add strategies classes and discount application trigger

--- app/main_oop.py
+++ app/main_oop.py
@@ -12,102 +12,71 @@ store1 = {
   'first_order_discount_value': None
 }
 
-class Store:
-  def __new__(cls, *args, **kwargs):
-    '''
-    Discounts subclasses factory
-    '''
-    if kwargs.get('delivery_discount_applicable') is True:
-      return super().__new__(DeliveryDiscount)
-    elif kwargs.get('first_order_discount_applicable') is True:
-      return super().__new__(FirstOrderDiscount)
-    else:
-      return super().__new__(NoDiscount)
+class DiscountStrategy:
+  def apply_discount(self, store):
+    return 0
+  # add s string representation of the class
+  def __str__(self):
+    return self.__class__.__name__
 
+class DeliveryDiscountStrategy(DiscountStrategy):
+  def apply_discount(self, store):
+    if store.delivery_discount_applicable and store.cart_sum() > store.cart_sum_threshold:
+      return store.regular_delivery_fee #the discount is the delivery fee
+    else:
+      return 0 #elso - no discount, diminished by 0
+
+class FirstOrderDiscountStrategy(DiscountStrategy):
+  def apply_discount(self, store):
+    if store.first_order_discount_applicable and store.order_count == 1:
+      return store.first_order_discount_value #the discount is the first order discount value
+    return 0 #elso - no discount, diminished by 0
+
+class Store:
   def __init__(self, 
               name, 
               price, 
               quantity, 
               regular_delivery_fee,
+              order_count = None,
+              delivery_discount_applicable = False,
+              cart_sum_threshold = None,
+              first_order_discount_applicable = False,
+              first_order_discount_value = None,
               **kwargs):
     self.name = name
     self.price = price
     self.quantity = quantity
     self.regular_delivery_fee = regular_delivery_fee
+    self.order_count = order_count
+    self.delivery_discount_applicable = delivery_discount_applicable
+    self.cart_sum_threshold = cart_sum_threshold
+    self.first_order_discount_applicable = first_order_discount_applicable
+    self.first_order_discount_value = first_order_discount_value
+    self.delivery_discount_strategy = DeliveryDiscountStrategy()
+    self.first_order_discount_strategy = FirstOrderDiscountStrategy()
+    self.discount_strategies = self.initialize_discount_strategies()
+
+  def initialize_discount_strategies(self):
+    strategies = []
+    if self.delivery_discount_applicable:
+      strategies.append(DeliveryDiscountStrategy())
+    if self.first_order_discount_applicable:
+      strategies.append(FirstOrderDiscountStrategy())
+    return strategies
 
   def cart_sum(self):
     return self.price * self.quantity
 
   def delivery_fee(self):
-      return self.regular_delivery_fee
+      return self.regular_delivery_fee -self.delivery_discount_strategy.apply_discount(self)
 
   def order_sum(self):
       return self.cart_sum() + self.delivery_fee()
 
   def deal_price(self):
-      return self.order_sum()
+      return self.order_sum() - self.first_order_discount_strategy.apply_discount(self)
   
-  # call class factory manually
-  # S1 = Store.store_subclasses_factory(store1)
-  @staticmethod
-  def store_subclasses_factory(store_dict):
-    """
-    This method creates a subclass of Store based on the store_dict keys.
-    """
-    if store_dict.get('delivery_discount_applicable') is True:
-      return DeliveryDiscount(**store_dict)
-    elif store_dict.get('first_order_discount_applicable') is True:
-      return FirstOrderDiscount(**store_dict)
-    else:
-      return NoDiscount(**store_dict)
-
-
-class NoDiscount(Store):
-  '''
-  Use basic calculation logic
-  '''
-  pass
-
-class DeliveryDiscount(Store):
-  '''
-  Apply delivery discount
-  '''
-  def __init__(self,
-              delivery_discount_applicable,
-              cart_sum_threshold,
-              **kwargs):
-    super().__init__(**kwargs) #initialise attributes and methods of the parent class
-    self.delivery_discount_applicable = delivery_discount_applicable
-    self.cart_sum_threshold = cart_sum_threshold
-  
-  def delivery_fee(self):
-    if self.delivery_discount_applicable and self.cart_sum() > self.cart_sum_threshold:
-      return 0
-    else:
-      return self.regular_delivery_fee
-
-class FirstOrderDiscount(Store):
-  '''
-  Apply first order discount logic.
-  '''
-  def __init__(self,
-              first_order_discount_applicable,
-              first_order_discount_value,
-              order_count,
-              **kwargs):
-    super().__init__(**kwargs) #initialise attributes and methods of the parent class
-    self.first_order_discount_applicable = first_order_discount_applicable
-    self.first_order_discount_value = first_order_discount_value
-    self.order_count = order_count
-
-def deal_price(self):
-  if self.first_order_discount_applicable and self.order_count == 1:
-    return self.order_sum() - self.first_order_discount_value
-  else:
-    return self.order_sum()
-
-
 S1 = Store(**store1)
+print(*S1.discount_strategies)
 print(S1.deal_price())
-
-
