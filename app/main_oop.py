e474949358d70528aa815660eea1759761eba9c7
Walter462 <74890413+Walter462@users.noreply.github.com>
2025-05-23 11:34:38 +0800

    feat(main_oop.py): add subclasses for specific discounts

--- app/main_oop.py
+++ app/main_oop.py
@@ -17,42 +17,86 @@ class Store:
               name, 
               price, 
               quantity, 
-              regular_delivery_fee, 
-              order_count, 
-              delivery_discount_applicable, 
-              cart_sum_threshold, 
-              first_order_discount_applicable, 
-              first_order_discount_value):
+              regular_delivery_fee,
+              **kwargs):
     self.name = name
     self.price = price
     self.quantity = quantity
     self.regular_delivery_fee = regular_delivery_fee
-    self.order_count = order_count
-    self.delivery_discount_applicable = delivery_discount_applicable  
-    self.cart_sum_threshold = cart_sum_threshold
-    self.first_order_discount_applicable = first_order_discount_applicable
-    self.first_order_discount_value = first_order_discount_value
 
   def cart_sum(self):
     return self.price * self.quantity
 
   def delivery_fee(self):
-    if self.delivery_discount_applicable and self.cart_sum() > self.cart_sum_threshold:
-      return 0
-    else:
       return self.regular_delivery_fee
 
   def order_sum(self):
       return self.cart_sum() + self.delivery_fee()
 
   def deal_price(self):
-    if self.first_order_discount_applicable and self.order_count == 1:
-      return self.order_sum() - self.first_order_discount_value
-    else:
       return self.order_sum()
+  
+  # call class factory manually
+  # S1 = Store.store_subclasses_factory(store1)
+  @staticmethod
+  def store_subclasses_factory(store_dict):
+    """
+    This method creates a subclass of Store based on the store_dict keys.
+    """
+    if store_dict.get('delivery_discount_applicable') is True:
+      return DeliveryDiscount(**store_dict)
+    elif store_dict.get('first_order_discount_applicable') is True:
+      return FirstOrderDiscount(**store_dict)
+    else:
+      return NoDiscount(**store_dict)
 
 
-S1 = Store(**store1)
+class NoDiscount(Store):
+  '''
+  Use basic calculation logic
+  '''
+  pass
+
+class DeliveryDiscount(Store):
+  '''
+  Apply delivery discount
+  '''
+  def __init__(self,
+              delivery_discount_applicable,
+              cart_sum_threshold,
+              **kwargs):
+    super().__init__(**kwargs)
+    self.delivery_discount_applicable = delivery_discount_applicable
+    self.cart_sum_threshold = cart_sum_threshold
+  
+  def delivery_fee(self):
+    if self.delivery_discount_applicable and self.cart_sum() > self.cart_sum_threshold:
+      return 0
+    else:
+      return self.regular_delivery_fee
+
+class FirstOrderDiscount(Store):
+  '''
+  Apply first order discount logic
+  '''
+  def __init__(self,
+              first_order_discount_applicable,
+              first_order_discount_value,
+              order_count,
+              **kwargs):
+    super().__init__(**kwargs)
+    self.first_order_discount_applicable = first_order_discount_applicable
+    self.first_order_discount_value = first_order_discount_value
+    self.order_count = order_count
+
+def deal_price(self):
+  if self.first_order_discount_applicable and self.order_count == 1:
+    return self.order_sum() - self.first_order_discount_value
+  else:
+    return self.order_sum()
+
+
+S1 = Store.store_subclasses_factory(store1)
 print(S1.deal_price())
 
 
